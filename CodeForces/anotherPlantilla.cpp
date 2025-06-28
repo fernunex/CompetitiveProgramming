f40a2a1f4d20bb14fa279fcf22c24b7473d97a5d
fernunex <fer.bsof@outlook.com>
2025-06-28 15:01:03 -0600

    This template looks interesting

---
+++ CodeForces/anotherPlantilla.cpp
@@ -0,0 +1,49 @@
+    /* 
+        Problem:
+        Name: 
+    */
+    
+    #include <bits/stdc++.h>
+    using namespace std;
+    
+    // ----------------- Typedefs -----------------
+    typedef long long ll;
+    typedef pair<int, int> pii;
+    typedef vector<int> vi;
+    typedef vector<ll> vll;
+    
+    // ----------------- Macros -----------------
+    #define fastIO() ios::sync_with_stdio(false); cin.tie(nullptr)
+    #define FOR(i, a, b) for (int i = a; i < b; ++i)
+    #define RFOR(i, a, b) for (int i = a; i >= b; --i)
+    #define all(x) (x).begin(), (x).end()
+    #define rall(x) (x).rbegin(), (x).rend()
+    #define pb push_back
+    #define ff first
+    #define ss second
+    #define in(v) for (auto &x : v) cin >> x
+    #define out(v) for (auto &x : v) cout << x << ' '; cout << '\n'
+    
+    // ----------------- Constants -----------------
+    const int MOD = 1e9 + 7;
+    const int INF = 1e9;
+    const ll LINF = 1e18;
+    
+    // ----------------- Solve Function -----------------
+    void solve() {
+        int n,k;
+        cin>>n>>k;
+
+    
+    }
+    
+    // ----------------- Main Function -----------------
+    int main() {
+        fastIO();
+        int t = 1;
+        cin >> t;
+        while (t--) {
+            solve();
+        }
+        return 0;
+    }
