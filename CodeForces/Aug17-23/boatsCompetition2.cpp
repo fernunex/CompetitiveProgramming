016c355e70d1d402e3b8e213c3f7b261ea150d20
fernunex <fer.bsof@outlook.com>
2025-08-19 20:34:52 -0600

    1 Problem 1200 Solved

---
+++ CodeForces/Aug17-23/boatsCompetition2.cpp
@@ -0,0 +1,48 @@
+//Problem: https://codeforces.com/contest/1399/problem/C
+// Name: C. Boats Competition
+// Author: Fer Nunez
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve(){
+    int n, val;
+    cin >> n;
+
+    map<int, int> m;
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        m[val] += 1;
+    }
+
+    int res = 0, count, count2;
+    for (int i = 2; i <= 100; i++){
+        count = 0;
+        count2 = 0;
+        for (const auto& par : m){
+                // cout << "Par.first: " << par.first << "--i:" << i << endl;
+            if (par.first > i) break;
+            if (par.first != i-par.first){
+                if (m.find(i-par.first) != m.end()){
+                    count += min(par.second, m[i-par.first]);
+                }
+            }
+            else{
+                count2 += par.second/2;
+            }
+        }
+        res = max(res, count/2 +count2);
+    }
+
+    cout << res << endl;
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int tt;
+    cin>>tt;
+    while(tt--) solve();
+    return 0;
+}
